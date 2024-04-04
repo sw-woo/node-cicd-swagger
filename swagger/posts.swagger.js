76edcfe00fd3f76edcfa02ae9749985875571006
sungwoo <swpheus1@gmail.com>
2024-04-04 16:08:02 +0900

    feat:post부분 수정

--- swagger/posts.swagger.js
+++ swagger/posts.swagger.js
@@ -6,13 +6,6 @@
  *      description: 모든 아티클 조회
  *      produces:
  *      - application/json
- *      parameters:
- *        - in: query
- *          name: 아티클 번호
- *          required: false
- *          schema:
- *            type: integer
- *            description: 게시글 번호
  *      responses:
  *       200:
  *        description: 게시글 조회 성공
