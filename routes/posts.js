57c4ccf02db57563d8c62a7ac93e89f46f039963
sungwoo <swpheus1@gmail.com>
2024-04-05 14:25:01 +0900

    feat:post라우터 부분 수정

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/all", (req, res) => {
-	res.render("posts", {title: "오늘도 즐겁게"});
+	res.render("posts.ejs", {title: "오늘도 즐겁게"});
 });
 
 module.exports = router;
