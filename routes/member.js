98fb431f794b0075c6aba485577d08c5ba289d33
sungwoo <swpheus1@gmail.com>
2024-04-05 15:52:32 +0900

    feat: member/all get타입 apl 추가

--- routes/member.js
+++ routes/member.js
@@ -6,4 +6,13 @@ router.get("/", (req, res) => {
 	res.render("posts");
 });
 
+router.get("/all", (req, res) => {
+	member = [
+		{id: 1, name: "sungwoo1"},
+		{id: 2, name: "sungwoo2"},
+	];
+
+	res.json(member);
+});
+
 module.exports = router;
