bdbc18ca3e40d510c663ff3b601b495162bce24a
sungwoo <swpheus1@gmail.com>
2024-04-05 17:52:00 +0900

    adminboard router

--- routes/adminBoard.js
+++ routes/adminBoard.js
@@ -6,4 +6,15 @@ router.get("/all", (req, res) => {
 	res.render("posts");
 });
 
+router.get("/:boardId", (req, res) => {
+	const boardId = req.params.boardId;
+
+	const result = {
+		boardId,
+		result: "OK",
+	};
+
+	res.render(result);
+});
+
 module.exports = router;
